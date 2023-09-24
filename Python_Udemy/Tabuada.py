a3151f5a6d80e48da0d89a6ca991b0847dc1de92
darkrepulsor <abraao.lima@live.com>
2023-09-23 23:04:12 -0300

    Multiplication Table

--- Python_Udemy/Tabuada.py
+++ Python_Udemy/Tabuada.py
@@ -1,11 +1,12 @@
-# Simples programa de tabuada para relembrar conceitos da linguagem python!
+# Simple multiplication program in python
 
-tabuada = int(input("Qual a tabuada que deseja ver na tela: "))
-lmtTabuada = int(input("Deseja a tabuada até quanto? Ex: 9 x 20 "))
+tabuada = int(input("Wich multiplication table you want to see in screen: "))
+lmtTabuada = int(
+    input("The multiplication table must go until... Ex: 9 x 20 "))
 i = 0
 
 while (i <= lmtTabuada):
     result = tabuada * i
     print(str(tabuada) + " x " + str(i) + " = " + str(result))
     i = i + 1
-print("\nObrgiado por usar o programa de tabuada!! \nAté mais!")
+print("\nThanks for using the mulplication table program!! \nUntil next time!")
